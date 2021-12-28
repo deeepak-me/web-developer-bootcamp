901e9e77db0762f4736264607c056db00fcb339c
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2021-12-29 00:57:06 +0530

    feat: js file created

---
+++ pokemon sprites demo/app.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('#container');
+
+const baseUrl = 'https://raw.githubusercontent.com/pokeAPI/sprites/master/sprites/pokemon/';
+
+for (let i = 0; i < 151; i++) {
+    const pokemon = document.createElement('div')
+    pokemon.classList.add('pokemon');
+
+    const label = document.createElement('span');
+    label.innerText = `${i}`;
+
+    const newImg = document.createElement('img');
+
+    newImg.src = `${baseUrl}${i}.png`
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+
+    container.appendChild(pokemon);
+}
