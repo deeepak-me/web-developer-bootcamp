950374682b2b7863adbe3592d62acff458b9fd90
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-01-08 21:17:47 +0530

    feat: updating js part

--- Random Color/app.js
+++ Random Color/app.js
@@ -3,12 +3,25 @@ const text = document.querySelector('h1');
 
 button.addEventListener('click', function() {
 
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    const newColor = `rgb(${r},${g},${b})`;
-
+    const newColor = makeRandColor();
     document.body.style.backgroundColor = newColor;
     text.innerText = newColor;
 
+
 })
+
+const makeRandColor = () => {
+
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    if (r < 155 && g < 155 && b < 155) {
+        text.style.color = "white";
+    } else {
+        text.style.color = 'black';
+    }
+
+    return `rgb(${r},${g},${b})`;
+
+}
