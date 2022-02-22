22df34b33f54a5ce8858f2297367c435f5ce8431
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-02-23 00:49:55 +0530

    feat: promise completed

--- Rainbow 2.0/app.js
+++ Rainbow 2.0/app.js
@@ -1,8 +1,16 @@
-const delayedColorchange(color, delay) => {
-    return new Promise(resolve, reject) => {
+const delayedColorchange = (color, delay) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             document.body.style.backgroundColor = color;
-            resolve()
+            resolve();
         }, delay)
-    }
+    })
 }
+
+delayedColorchange('red', 1000)
+    .then(() => delayedColorchange('orange', 1000))
+    .then(() => delayedColorchange('yellow', 1000))
+    .then(() => delayedColorchange('green', 1000))
+    .then(() => delayedColorchange('blue', 1000))
+    .then(() => delayedColorchange('indigo', 1000))
+    .then(() => delayedColorchange('violet', 1000))
